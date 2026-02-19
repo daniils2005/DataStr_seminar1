b558499faef896265631e117db7aef77afe0a002
daniils2005 <daniils.strals@gmail.com>
2026-02-19 11:37:32 +0200

    Add: added variable in ArrayList class

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,5 +1,13 @@
 package datastr;
 
 public class MyArrayList {
-
+	//mainigie
+	private char[] list;
+	private int howManyElements = 0;
+	private final int DEFAULT_SIZE = 0;
+	private int size = DEFAULT_SIZE;
+	
+	//konstruktori
+	
+	//funckijas
 }
